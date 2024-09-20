bfa329e6a1749ff569accf13179497f158833f05
Davide Giliberti <gilo.davide@hotmail.it>
2024-09-20 12:56:39 +0200

    progetto creato

---
+++ src/main/java/davidegiliberti/progetto_20_9_24/Progetto20924Application.java
@@ -0,0 +1,13 @@
+package davidegiliberti.progetto_20_9_24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Progetto20924Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Progetto20924Application.class, args);
+	}
+
+}
---
+++ src/test/java/davidegiliberti/progetto_20_9_24/Progetto20924ApplicationTests.java
@@ -0,0 +1,13 @@
+package davidegiliberti.progetto_20_9_24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Progetto20924ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
